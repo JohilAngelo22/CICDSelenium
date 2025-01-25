4920b3018a4ed71ae82b03e20a0c5d0ff653dfed
Johil Angelo <johil_angelo@epam.com>
2025-01-25 16:53:51 +0530

    Add project files.

---
+++ BaseTest.cs
@@ -0,0 +1,59 @@
+﻿using Allure.Commons;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Edge;
+using OpenQA.Selenium.Firefox;
+
+namespace CICDSelenium
+{
+    public class BaseTest
+    {
+        protected IWebDriver? Driver;
+
+        public BaseTest()
+        {
+            string browser = "chrome"; // you can get this from environment variable or config file
+
+            Driver = browser.ToLower() switch
+            {
+                "chrome" => new ChromeDriver(),
+                "firefox" => new FirefoxDriver(),
+                "edge" => new EdgeDriver(),
+                _ => throw new NotImplementedException("Browser not supported."),
+            };
+            Driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+
+        }
+
+        // Cleanup after each test
+        [TearDown]
+        public void Teardown()
+        {
+            Driver?.Quit();
+        }
+
+        public void NavigateToApplicationUrl()
+        {
+            Driver?.Navigate().GoToUrl("https://demo.automationtesting.in/Register.html");
+            Driver?.Manage().Window.Maximize();
+        }
+
+        public void TakeScreenshot(string testName)
+        {
+            ITakesScreenshot screenshotDriver = (ITakesScreenshot)Driver!;
+            Screenshot screenshot = screenshotDriver.GetScreenshot();
+            string screenshotPath = Path.Combine("screenshots", $"{testName}_{DateTime.Now:yyyyMMddHHmmss}.png");
+
+            if (!Directory.Exists("screenshots"))
+            {
+                Directory.CreateDirectory("screenshots");
+            }
+
+            File.WriteAllBytes(screenshotPath, screenshot.AsByteArray);
+
+            // Attach screenshot to Allure report
+            AllureLifecycle.Instance.AddAttachment(testName, "image/png", screenshotPath);
+        }
+    }
+}
---
+++ RegistrationTest.cs
@@ -0,0 +1,41 @@
+﻿using Allure.Net.Commons;
+using Allure.NUnit;
+using Allure.NUnit.Attributes;
+using NUnit.Framework;
+using UICore;
+
+namespace CICDSelenium
+{
+    [TestFixture]
+    [AllureNUnit]
+    public class RegistrationTest : BaseTest
+    {
+        readonly RegistrationPage registrationPage;
+
+        [SetUp]
+        public void NavigateToApplication()
+        {
+            NavigateToApplicationUrl();
+        }
+        public RegistrationTest():base()
+        {
+            registrationPage = new(Driver!);
+        }
+
+        [Test]
+        [AllureSeverity(SeverityLevel.critical)]  // Allure severity annotation
+        [AllureOwner("TesterName")]
+        [AllureSuite("Selenium Tests")]
+        public void RegistrationSubmissionTests()
+        {
+            registrationPage.EnterFirstAndLastName("Johil", "Angelo");
+            registrationPage.EnterAddress("Kallaravilai");
+            registrationPage.EnterEmail("automationcheck@gmail.com");
+            registrationPage.ClickOnMaleGender();
+            registrationPage.EnterPhoneNumber("9442738206");
+            registrationPage.ClickOnSubmitButton();
+
+            var hjhj = registrationPage.CountryToolTipText();
+        }
+    }
+}
